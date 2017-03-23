8f49ed04270d826ee38769a943bc2e1866b135f9
krishnasharma <krishnasharma@192.168.0.7>
2017-03-23 12:54:21 -0500

    Creating New Spring Boot Web App

---
+++ src/main/java/com/SpringBootSample/Controllers/SampleController.java
@@ -0,0 +1,24 @@
+package com.SpringBootSample.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.SpringBootSample.Models.Person;
+
+@Controller
+public class SampleController {
+
+	    @RequestMapping("/person")
+	    public ModelAndView helloWorld() {
+
+	        ModelAndView model = new ModelAndView("personview");
+	        Person person = new Person();
+	        person.setFirstName("Kanishk");
+	        person.setLastName("Sharma");
+	        person.setAge(2);
+	        model.addObject("person", person);
+	        return model;
+	    }
+
+}
